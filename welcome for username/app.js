3a53fe1fc3978cf4b88c87c96d2cb003f247a251
ionelaalbu <154548267+ionelaalbu@users.noreply.github.com>
2024-05-10 17:19:11 +0200

    Add files via upload

---
+++ welcome for username/app.js
@@ -0,0 +1,12 @@
+
+
+function welcome(){
+    const user = document.getElementById('username').value
+    const place = document.getElementById('exemple');
+    const bt = document.getElementById('btn');
+    if(user === username.value){
+        place.textContent =`hi ${user} welcome`
+    }
+}
+
+
